41a8ffe2ae33503de315628b6fda324d71215fd1
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2026-07-29 12:05:46 -0500

    Fix test program

--- circuitpython/test.py
+++ circuitpython/test.py
@@ -14,8 +14,8 @@ device = zero_stomp.ZeroStomp()
 device.mix = 0.5
 
 if TYPE == TYPE_EFFECT:
-    import audiodelays
-    effect = audiodelays.Echo(
+    from audiodelays import Echo
+    effect = Echo(
         max_delay_ms=100,
         delay_ms=200,
         mix=1.0,
@@ -28,12 +28,15 @@ if TYPE == TYPE_EFFECT:
         buffer_size=zero_stomp.BUFFER_SIZE,
     )
     
-    effect.play(device.i2s, loop=True)
-    device.i2s.play(effect, loop=True)
+    device.audio_out.play(
+        effect.play(
+            device.audio_in
+        )
+    )
     
 elif TYPE == TYPE_MIXER:
-    import audiomixer
-    mixer = audiomixer.Mixer(
+    from audiomixer import Mixer
+    mixer = Mixer(
         voice_count=1,
 
         sample_rate=zero_stomp.SAMPLE_RATE,
@@ -42,14 +45,14 @@ elif TYPE == TYPE_MIXER:
         samples_signed=zero_stomp.SAMPLES_SIGNED,
         buffer_size=zero_stomp.BUFFER_SIZE,
     )
-    device.i2s.play(mixer)
-    mixer.voice[0].play(device.i2s, loop=True)
+    device.audio_out.play(mixer)
+    mixer.voice[0].play(device.audio_in)
     
 else:
-    device.i2s.play(device.i2s)
+    device.audio_out.play(device.audio_in)
 
 if TEST:
     b = array.array("h", [0] * 256)
     while True:
-        device.i2s.record(b, len(b))
+        device.audio_in.record(b, len(b))
         print(np.max(np.array(b, dtype=np.int16)))
